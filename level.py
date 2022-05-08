4f09df6559118aaa61bab081969d8abb54700332
Wesley Bernard <wesley.bernard64@gmail.com>
2022-05-08 18:05:52 -0400

    printing row index along with row

--- level.py
+++ level.py
@@ -11,7 +11,8 @@ class Level:
         # sprite setup
         self.create_map()
     def create_map(self):
-        for row in WORLD_MAP:
+        for row_index,row in enumerate(WORLD_MAP):
+            print(row_index)
             print(row)
     def run(self):
         # update and draw the game
