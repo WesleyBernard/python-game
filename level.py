44f2a8f263b82d0f2c605171e09f1899218b72cf
Wesley Bernard <wesley.bernard64@gmail.com>
2022-05-08 18:51:28 -0400

    Shuriken walls omegalol

--- level.py
+++ level.py
@@ -1,5 +1,7 @@
 import pygame
 from settings import *
+from tile import Tile
+from player import Player
 
 class Level:
     def __init__(self):
@@ -12,8 +14,12 @@ class Level:
         self.create_map()
     def create_map(self):
         for row_index,row in enumerate(WORLD_MAP):
-            print(row_index)
-            print(row)
+            for col_index, col in enumerate(row):
+                x = col_index * TILESIZE
+                y = row_index * TILESIZE
+                if col == 'x':
+                    Tile((x,y), [self.visible_sprites])
+
     def run(self):
         # update and draw the game
-        pass
+        self.visible_sprites.draw(self.display_surface)
